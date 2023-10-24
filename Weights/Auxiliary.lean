bd9a6751eead54a104e70c00359d5b04b14ac5fa
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-10-24 22:39:42 +0200

    golf lt_two_mul_of_div_two_lt

--- Weights/Auxiliary.lean
+++ Weights/Auxiliary.lean
@@ -24,8 +24,8 @@ lemma proportional {a b c d : ℕ} (h : a * d = b * c) (h' : Nat.Coprime a b) :
     obtain rfl := mul_left_cancel₀ H h
     exact ⟨d₁, by simp [mul_comm d₁]⟩
 
-lemma eq_and_eq_of_coprime_coprime_mul_eq_mul {a b c d : ℕ} (hab : Nat.Coprime a b) (hcd : Nat.Coprime c d)
-    (h : a * d = b * c) :
+lemma eq_and_eq_of_coprime_coprime_mul_eq_mul {a b c d : ℕ} (hab : Nat.Coprime a b)
+    (hcd : Nat.Coprime c d) (h : a * d = b * c) :
     a = c ∧ b = d :=
   ⟨Nat.dvd_antisymm (hab.dvd_of_dvd_mul_left <| Dvd.intro d h)
                     (hcd.dvd_of_dvd_mul_right <| Dvd.intro_left b h.symm),
@@ -35,12 +35,11 @@ lemma eq_and_eq_of_coprime_coprime_mul_eq_mul {a b c d : ℕ} (hab : Nat.Coprime
 lemma eq_one_of_coprime_mul_mul {a b c : ℕ} (h : Nat.Coprime (a * b) (a * c)) : a = 1 :=
   (Nat.coprime_self a).mp <| Nat.Coprime.coprime_mul_right <| Nat.Coprime.coprime_mul_right_right h
 
+open Nat in
 lemma lt_two_mul_of_div_two_lt {a d : ℕ} (h : d / 2 < a) : d < 2 * a := by
-  have : d = 2 * (d / 2) + d % 2 := (Nat.div_add_mod d 2).symm
-  rw [this]
-  change d / 2 + 1 ≤ a at h
-  change 2 * (d / 2) + d % 2 + 1 ≤ 2 * a
-  have : d % 2 < 2 := Nat.mod_lt d (zero_lt_two)
-  linarith
+  rw [lt_iff_add_one_le, ← div_add_mod d 2, add_right_comm]
+  rw [lt_iff_add_one_le, ← mul_le_mul_left (a := 2) zero_lt_two] at h
+  rw [mul_add, two_mul 1, ← add_assoc] at h
+  exact (add_le_add_left (lt_succ.mp <| mod_lt d (zero_lt_two)) _).trans h
 
 end Weight
